2f1f02e5aa04b82a360253af0842dd6550a1b253
João Gomes <joaosilgo96@gmail.com>
2019-01-13 04:42:28 +0000

    Layout Pdf

--- AlbergueAnimal/Models/DbInitializer.cs
+++ AlbergueAnimal/Models/DbInitializer.cs
@@ -41,16 +41,16 @@ namespace AlbergueAnimal.Models
                 context.SaveChanges();
             }
 
-            /*if (!context.Animal.Any()) //RacaId começa no 1
-            {          
-                context.Animal.Add(new Animal { RacaId = 1, Nome = "Bobby", Genero = "Macho", Cor = "Preto", DataEntrada = new DateTime(1998, 04, 30), DataVacina = new DateTime(1998, 04, 30), DataNascimento = new DateTime(1998, 03, 30), Arquivado = false });
-                context.Animal.Add(new Animal { RacaId = 1, Nome = "Peludo", Genero = "Macho", Cor = "Castanho", DataEntrada = new DateTime(2015, 10, 22), DataVacina = new DateTime(2016, 05, 10), DataNascimento = new DateTime(2014, 09, 05), Arquivado = false });
-                context.Animal.Add(new Animal { RacaId = 2, Nome = "Abelinda", Genero = "Fêmea", Cor = "Branco", DataEntrada = new DateTime(2015, 12, 03), DataVacina = new DateTime(2016, 02, 12), DataNascimento = new DateTime(2013, 07, 01), Arquivado = false });
-                context.Animal.Add(new Animal { RacaId = 3, Nome = "Felpudo", Genero = "Macho", Cor = "Cinzento", DataEntrada = new DateTime(2018, 09, 15), DataVacina = new DateTime(2017, 11, 16), DataNascimento = new DateTime(2016, 02, 15), Arquivado = false });
-                context.Animal.Add(new Animal { RacaId = 4, Nome = "Cinula", Genero = "Fêmea", Cor = "Castanho", DataEntrada = new DateTime(2017, 04, 11), DataVacina = new DateTime(2018, 12, 04), DataNascimento = new DateTime(2015, 10, 26), Arquivado = false });
+            //if (!context.Animal.Any()) //RacaId começa no 1
+            //{          
+            //    context.Animal.Add(new Animal { RacaId = 1, Nome = "Bobby", Genero = "Macho", Cor = "Preto", DataEntrada = new DateTime(1998, 04, 30), DataVacina = new DateTime(1998, 04, 30), DataNascimento = new DateTime(1998, 03, 30), Arquivado = false });
+            //    context.Animal.Add(new Animal { RacaId = 1, Nome = "Peludo", Genero = "Macho", Cor = "Castanho", DataEntrada = new DateTime(2015, 10, 22), DataVacina = new DateTime(2016, 05, 10), DataNascimento = new DateTime(2014, 09, 05), Arquivado = false });
+            //    context.Animal.Add(new Animal { RacaId = 2, Nome = "Abelinda", Genero = "Fêmea", Cor = "Branco", DataEntrada = new DateTime(2015, 12, 03), DataVacina = new DateTime(2016, 02, 12), DataNascimento = new DateTime(2013, 07, 01), Arquivado = false });
+            //    context.Animal.Add(new Animal { RacaId = 3, Nome = "Felpudo", Genero = "Macho", Cor = "Cinzento", DataEntrada = new DateTime(2018, 09, 15), DataVacina = new DateTime(2017, 11, 16), DataNascimento = new DateTime(2016, 02, 15), Arquivado = false });
+            //    context.Animal.Add(new Animal { RacaId = 4, Nome = "Cinula", Genero = "Fêmea", Cor = "Castanho", DataEntrada = new DateTime(2017, 04, 11), DataVacina = new DateTime(2018, 12, 04), DataNascimento = new DateTime(2015, 10, 26), Arquivado = false });
 
-                context.SaveChanges();
-            }*/
+            //    context.SaveChanges();
+            //}
         }
     }
 }
