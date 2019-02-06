07f59a9829d446eefb909a8fa00a236617989a55
João Gomes <joaosilgo96@gmail.com>
2019-02-06 14:38:32 +0000

    change branch

---
+++ AlbergueAnimal/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbergueAnimal.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlbergueAnimal.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context);
+        }
+    }
+}
